80dd96ec9f3ba61cfbdc77feece3f437e73909fb
Óscar Vega <oscarvegaherrera59@gmail.com>
2017-11-07 18:09:46 +0100

    Comprueba los datos de entrada

--- calculadora.php
+++ calculadora.php
@@ -7,6 +7,8 @@
     <body>
         <?php
 
+        $op1 = $op2 = $op = null;
+
         $op1 = $_GET["op1"];
         $op2 = $_GET["op2"];
         $op = $_GET["op"];
@@ -30,14 +32,23 @@
                     $resul = $op1 / $op2;
                     break;
 
-                default:
-                    $resul = "Error: Operación no válida.";
-                    break;
             }
             return $resul;
         }
 
         ?>
-        <p>El resultado es <?= calcula($op1, $op2, $op) ?></p>
+        <?php if (isset($op1, $op2, $op)): ?>
+            <?php if (is_numeric($op1) && is_numeric($op2)): ?>
+                <?php if (in_array($op, ['-', '+', '*', '/'])): ?>
+                    <p>El resultado es <?= calcula($op1, $op2, $op) ?></p>
+                <?php else: ?>
+                    <h3>Error: Operación no válida</h3>
+                <?php endif ?>
+            <?php else: ?>
+                <h3>Error: Los parámetros deben ser numéricos.</h3>
+            <?php endif ?>
+        <?php else: ?>
+            <h3>Error: Los parámetros son obligatorios</h3>
+        <?php endif ?>
     </body>
 </html>
