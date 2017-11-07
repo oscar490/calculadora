2fb0652d3c498a7774289bdf5c3f9c66415282d8
Óscar Vega <oscarvegaherrera59@gmail.com>
2017-11-07 19:42:05 +0100

    Funciona y pinta el resultadoen un campo de textp

--- calculadora.php
+++ calculadora.php
@@ -6,14 +6,25 @@
     </head>
     <body>
         <?php
-
+        $ret = "";
         $op1 = $op2 = $op = null;
+        extract($_GET, EXTR_IF_EXISTS);
 
-        $op1 = $_GET["op1"];
-        $op2 = $_GET["op2"];
-        $op = $_GET["op"];
 
-        function calcula($op1, $op2, $op)
+
+
+
+
+
+
+        /**
+         * Realiza una operación aritmética
+         * @param  float $op1 primer operando
+         * @param  float $op2 segundo operando
+         * @param  string $op  tercer operando
+         * @return float      El resultado de la operación.
+         */
+        function calcula(float $op1, float $op2, string $op)
         {
             switch ($op) {
                 case '+':
@@ -40,15 +51,33 @@
         <?php if (isset($op1, $op2, $op)): ?>
             <?php if (is_numeric($op1) && is_numeric($op2)): ?>
                 <?php if (in_array($op, ['-', '+', '*', '/'])): ?>
-                    <p>El resultado es <?= calcula($op1, $op2, $op) ?></p>
+                    <?php $ret = calcula($op1, $op2, $op) ?>
                 <?php else: ?>
                     <h3>Error: Operación no válida</h3>
                 <?php endif ?>
             <?php else: ?>
                 <h3>Error: Los parámetros deben ser numéricos.</h3>
             <?php endif ?>
-        <?php else: ?>
-            <h3>Error: Los parámetros son obligatorios</h3>
         <?php endif ?>
+
+        <form action="calculadora.php"  method="get">
+            <label for="op1">Primer operando</label>
+            <input type="text" id="op1" name="op1" value="<?= $op1 ?>" /><br/>
+
+            <label for="op2">Segundo operando</label>
+            <input type="text" id ="op2" name="op2" value="<?= $op2 ?>"/><br />
+
+            <label for="resultado">Resultado</label>
+            <input type="text" id="resultado"  value="<?= $ret ?>"  /><br />
+
+            <input type="submit" value="Calcular"  />
+
+            <select name="op">
+                <option value="+">Suma</option>
+                <option value="-">Resta</option>
+                <option value="*">Multiplicar</option>
+                <option value="/">Dividir</option>
+            </select>
+        </form>
     </body>
 </html>
